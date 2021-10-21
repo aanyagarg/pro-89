651388b8bbf53fcde26ee18321f4a09bc575e05f
aanyagarg <73021607+aanyagarg@users.noreply.github.com>
2021-10-21 10:53:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDjDOBzuMi94gCogKgCsFuIKnFr4_YqT_k",
+  authDomain: "spectagram-ee75e.firebaseapp.com",
+  projectId: "spectagram-ee75e",
+  storageBucket: "spectagram-ee75e.appspot.com",
+  messagingSenderId: "405876617900",
+  appId: "1:405876617900:web:634f7290a93acff78f6509"
+};
